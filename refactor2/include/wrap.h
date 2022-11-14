4945bc35738b56572596ef5494211acc9c20a9eb
hermin9804 <hermin9804@gmail.com>
2022-11-14 17:42:54 +0900

    wrap.h include guard

--- refactor2/include/wrap.h
+++ refactor2/include/wrap.h
@@ -1,6 +1,11 @@
+#ifndef WRAP_H
+#define WRAP_H
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/syslimits.h>
 
 void *wrap_malloc(size_t size);
 int open(const char *pathname, int flags);
+
+#endif
