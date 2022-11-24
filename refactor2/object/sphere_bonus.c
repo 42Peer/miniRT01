bab0a18e74045d957454eac3a11419a1164112da
namzisun <namzisun@gmail.com>
2022-11-24 15:03:01 +0900

    update gold ball texture

--- refactor2/object/sphere_bonus.c
+++ refactor2/object/sphere_bonus.c
@@ -10,7 +10,7 @@ void	sphere(t_scene *scene, char **data)
 	sphere->radius_pow_2 = pow(sphere->radius, 2);
 	sphere->color = parse_color(data[3]);
 	sphere->texture.img = mlx_xpm_file_to_image(scene->mlx.mlx, \
-		"./asset/earthmap4k.xpm", &sphere->texture.img_w, \
+		"./asset/gold_ball_texture.xpm", &sphere->texture.img_w, \
 		&sphere->texture.img_h);
 	if (sphere->texture.img == NULL)
 		exit_with_error("unvalid xpm file\n");
@@ -18,7 +18,7 @@ void	sphere(t_scene *scene, char **data)
 	sphere->texture.img, &sphere->texture.bits_per_pixel, \
 	&sphere->texture.line_length, &sphere->texture.endian);
 	sphere->bump.img = mlx_xpm_file_to_image(scene->mlx.mlx, \
-		"./asset/earthbumpman2.xpm", &sphere->bump.img_w, &sphere->bump.img_h);
+		"./asset/gold_ball_bump.xpm", &sphere->bump.img_w, &sphere->bump.img_h);
 	if (sphere->bump.img == NULL)
 		exit_with_error("unvalid xpm file\n");
 	sphere->bump.addr = (unsigned char *)mlx_get_data_addr(sphere->bump.img, \
